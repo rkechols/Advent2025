dbc88ec3a538facf24bf67101b7781e8c1ace36b
Ryan Echols <rkechols21@gmail.com>
2025-11-30 23:29:12 -0700

    template

---
+++ template.py
@@ -0,0 +1,17 @@
+import re
+
+from utils import get_input_file_path
+
+
+def main():
+    with open(get_input_file_path(), "r", encoding="utf-8") as f:
+        for line_number, line in enumerate(f, start=1):
+            try:
+                pass
+            except Exception:
+                print(f"ERROR on input file line {line_number}")
+                raise
+
+
+if __name__ == "__main__":
+    main()
