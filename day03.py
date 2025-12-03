8728661b0217c576b45070094b907fe9fd98a2db
Ryan Echols <ryan.echols@trunk.tools>
2025-12-02 22:49:06 -0700

    solved day 3

--- day03.py
+++ day03.py
@@ -1,37 +1,45 @@
+from collections.abc import Sequence
+
 from utils import get_input_file_path
 
 
 DIGITS_DESCENDING = [9, 8, 7, 6, 5, 4, 3, 2, 1]
 
 
-def max_joltage_in_line(digits: list[int]):
-    for target_digit_a in DIGITS_DESCENDING:
-        try:
-            i = digits.index(target_digit_a, 0, -1)
-        except ValueError:
-            continue
-        for target_digit_b in DIGITS_DESCENDING:
+def max_joltage_in_line(digits: Sequence[int], *, k: int):
+    n = len(digits)
+    previous_chosen_index = -1
+    chosen_digits: list[int] = []
+    for i in range(k):
+        k_after = k - i - 1
+        for target_digit in DIGITS_DESCENDING:
             try:
-                digits.index(target_digit_b, i + 1)
+                target_digit_index = digits.index(target_digit, previous_chosen_index + 1, n - k_after)
             except ValueError:
                 continue
-            max_joltage = int(f"{target_digit_a}{target_digit_b}")
-            return max_joltage
-    raise ValueError("Goofed when finding max joltage in line")
+            chosen_digits.append(target_digit)
+            previous_chosen_index = target_digit_index
+            break
+        else:
+            raise ValueError(f"Goofed when finding max joltage")
+    max_joltage = int("".join(str(d) for d in chosen_digits))
+    return max_joltage
 
 
 def main():
-    joltage_sum = 0
+    joltage_sum_from_2 = 0
+    joltage_sum_from_12 = 0
     with open(get_input_file_path(3), "r", encoding="utf-8") as f:
         for line_number, line in enumerate(f, start=1):
             try:
                 digits = list(map(int, line.strip()))
-                max_joltage = max_joltage_in_line(digits)
-                joltage_sum += max_joltage
+                joltage_sum_from_2 += max_joltage_in_line(digits, k=2)
+                joltage_sum_from_12 += max_joltage_in_line(digits, k=12)
             except Exception:
                 print(f"ERROR on input file line {line_number}")
                 raise
-    print(f"{joltage_sum = }")
+    print(f"{joltage_sum_from_2 = }")
+    print(f"{joltage_sum_from_12 = }")
 
 
 if __name__ == "__main__":
