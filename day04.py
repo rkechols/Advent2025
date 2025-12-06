9b6a294ca00353d03fc796c9b2005feb268e2b75
Ryan Echols <ryan.echols@trunk.tools>
2025-12-06 00:19:23 -0700

    define GridSolver in utils

--- day04.py
+++ day04.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from utils import get_input_file_path
+from utils import get_input_file_path, GridSolver, Loc
 
 SYMBOL_TO_BOOL = {
     "@": True,  # roll
@@ -9,34 +9,18 @@ SYMBOL_TO_BOOL = {
 UNREMOVABLE_THRESHOLD = 4
 
 
-class Solver:
-    def __init__(self, grid: np.ndarray):
-        self.grid = np.pad(grid, 1)  # effectively makes a copy
-
-    @property
-    def n_rows(self) -> int:
-        return self.grid.shape[0]
-
-    @property
-    def n_cols(self) -> int:
-        return self.grid.shape[1]
-
+class Solver(GridSolver):
     def count_neighbors(self, i: int, j: int) -> int:
         count = 0
-        for i_shift in [-1, 0, 1]:
-            i_new = i + i_shift
-            for j_shift in [-1, 0, 1]:
-                if i_shift == 0 and j_shift == 0:
-                    continue  # don't look at the center
-                j_new = j + j_shift
-                if self.grid[i_new, j_new]:
-                    count += 1
+        for loc in self.iter_8_neighbors(i, j):
+            if self.grid[loc]:
+                count += 1
         return count
 
-    def get_removables(self) -> set[tuple[int, int]]:
-        removables: set[tuple[int, int]] = set()
-        for i in range(1, self.n_rows - 1):
-            for j in range(1, self.n_cols - 1):
+    def get_removables(self) -> set[Loc]:
+        removables: set[Loc] = set()
+        for i in range(self.n_rows):
+            for j in range(self.n_cols):
                 if not self.grid[i, j]:
                     continue
                 n_neighbors = self.count_neighbors(i, j)
@@ -44,8 +28,8 @@ class Solver:
                     removables.add((i, j))
         return removables
 
-    def iteratively_remove_all(self) -> set[tuple[int, int]]:
-        removed: set[tuple[int, int]] = set()
+    def iteratively_remove_all(self) -> set[Loc]:
+        removed: set[Loc] = set()
         while True:
             new_removables = self.get_removables()
             if len(new_removables) == 0:
--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
+from collections.abc import Generator
 from pathlib import Path
 from typing import Iterable
+
+import numpy as np
 
 INPUTS_DIR = Path(__file__).parent / "inputs"
 
@@ -13,3 +16,55 @@ def product(values: Iterable[int]) -> int:
     for value in values:
         prod *= value
     return prod
+
+
+Loc = tuple[int, int]
+
+
+class GridSolver:
+    def __init__(self, grid: np.ndarray):
+        super().__init__()
+        if len(grid.shape) != 2:
+            raise ValueError(f"Grid must be 2-dimensional, but shape was {grid.shape}")
+        self.grid = grid
+
+    @property
+    def n_rows(self) -> int:
+        return self.grid.shape[0]
+
+    @property
+    def n_cols(self) -> int:
+        return self.grid.shape[1]
+
+    def row_in_bounds(self, i: int) -> bool:
+        return 0 <= i < self.n_rows
+
+    def col_in_bounds(self, j: int) -> bool:
+        return 0 <= j < self.n_cols
+
+    def loc_in_bounds(self, i: int, j: int) -> bool:
+        return self.row_in_bounds(i) and self.col_in_bounds(j)
+
+    def iter_4_neighbors(self, i: int, j: int) -> Generator[Loc]:
+        for i_shift, j_shift in [
+            (0, 1),
+            (1, 0),
+            (0, -1),
+            (-1, 0),
+        ]:
+            i_new = i + i_shift
+            j_new = j + j_shift
+            if self.loc_in_bounds(i_new, j_new):
+                yield i_new, j_new
+
+    def iter_8_neighbors(self, i: int, j: int) -> Generator[Loc]:
+        for i_shift in [-1, 0, 1]:
+            i_new = i + i_shift
+            if not self.row_in_bounds(i_new):
+                continue
+            for j_shift in [-1, 0, 1]:
+                if i_shift == 0 and j_shift == 0:
+                    continue  # skip the center
+                j_new = j + j_shift
+                if self.col_in_bounds(j_new):
+                    yield i_new, j_new
